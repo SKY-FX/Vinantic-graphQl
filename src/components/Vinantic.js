dc0d36fd75cdf8100cf7b00357b1108a78be52e1
Chabaud <dsp_fx@hotmail.com>
2023-05-30 13:11:19 +0200

    add some meta data and input design

--- src/components/Vinantic.js
+++ src/components/Vinantic.js
@@ -11,27 +11,55 @@ const Vinantic = () => {
   } = useVinantic();
 
   return (
-    <div>
-      <h1>Vinantic</h1>
+    <div className="flex flex-col bg-gray-50 px-5">
+      <div className="my-10 font-tangerine font-thin text-9xl text-center">Vinantic</div>
 
-      <div>
-        <input
-          type="text"
-          value={searchText}
-          onChange={handleSearchChange}
-          placeholder="Recherche..."
-        />
-        <select value={sortBy} onChange={handleSortChange}>
-          <option value="">Trier par...</option>
-          <option value="year">Année</option>
-          <option value="price">Prix</option>
-          <option value="name">Nom</option>
-        </select>
+      <div className="flex flex-row">
+        <div className="relative w-64">
+          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg
+              aria-hidden="true"
+              className="w-5 h-5 text-gray-500 dark:text-gray-400"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              ></path>
+            </svg>
+          </div>
+          <input
+            type="search"
+            id="search"
+            className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            value={searchText}
+            onChange={handleSearchChange}
+            placeholder="Recherche..."
+          />
+        </div>
+
+        <div className="ml-10 w-64">
+          <select
+            value={sortBy}
+            onChange={handleSortChange}
+            className="h-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          >
+            <option selected>Aucun tri</option>
+            <option value="year">Année</option>
+            <option value="price">Prix</option>
+            <option value="name">Nom</option>
+          </select>
+        </div>
       </div>
 
-      <div className="grid gap-5 2xl:grid-cols-5 xl:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1">
+      <div className="grid gap-5 2xl:grid-cols-5 xl:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1 mt-10">
         {filteredWinesList.length !== 0 &&
-          filteredWinesList.map(wine => (
+          filteredWinesList.map((wine) => (
             <div key={wine.id} className="rounded-3xl shadow-md">
               <div className="bg-stone-100 flex flex-col border rounded-3xl h-full justify-between">
                 <div className="flex flex-col items-center m-10">
